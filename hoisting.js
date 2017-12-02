b50e429324cc012b47290319ae78d385bec42efe
Gunu Shrestha <realgunu@gmail.com>
2017-12-02 06:23:57 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -21,10 +21,11 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+
 
   function sayMy() {
     console.log(name);
     var name = "Kristin";
+    sayMy();
   }
 }
